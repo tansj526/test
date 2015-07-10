da0c0162e2215c6498da11bb6ecd90badf9c051e
tansj526 <tan_feel@163.com>
2015-07-11 00:10:16 +0800

    change3

--- drag.js
+++ drag.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 function drag(id){
-	var obj = document.getElementById(id);//
+	var obj = document.getElementById(id);////
 	var disX = 0;
 	var disY = 0;
 	obj.onmousedown = function(ev){
